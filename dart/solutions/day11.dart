d2ca494197645ee8bcaa329ef24c8646a5500272
Srishan Shakya <csunshakya@gmail.com>
2025-12-11 15:13:01 +0545

    refactor: day 11 part 2 solution with comments

--- dart/solutions/day11.dart
+++ dart/solutions/day11.dart
@@ -41,58 +41,63 @@ class Day11 extends GenericDay {
     const fft = 'fft';
     const out = 'out';
     final connections = parseInput();
-    final closure = transitiveClosure(
-      connections.keys,
-      (key) => connections[key] ?? [],
-    );
-    final waysFromDacToOut = getPaths(
-      connections,
-      from: dac,
-      to: out,
-      closure: closure,
-    );
-    final waysFromFftToDac = getPaths(
-      connections,
-      from: fft,
-      to: dac,
-      closure: closure,
-    );
-    final waysFromSvrToFft = getPaths(
-      connections,
-      from: serverRack,
-      to: fft,
-      closure: closure,
-    );
-    return waysFromSvrToFft * waysFromFftToDac * waysFromDacToOut;
+    final closure = transitiveClosure([serverRack], (key) {
+      return connections[key] ?? [];
+    });
+    // There are only two possible ways to get from 'svr' to 'out' while going
+    // through both dac and fft:
+    // 1. svr -> dac -> fft -> out
+    // 2. svr -> fft -> dac -> out
+    // Out of which only one way can be valid for an input,
+    // otherwise we hit a cycle between dac and fft hence creating infinite ways
+    // to get to `out` by going from dac to fft/fft to dac many number of times.
+    final possiblePathsOrder = [
+      [serverRack, dac, fft, out],
+      [serverRack, fft, dac, out],
+    ];
+
+    // Either there will be no path from dac to fft or from fft to dac in our
+    // transitive closure leaving behind a single `validPath`.
+    // In my input and the example, only fft can go to dac while dac cannot go
+    // to fft. I don't know if that is always true, but now the code handles
+    // both cases.
+    final [validPath] = possiblePathsOrder.map((e) => e.adjacentPairs).where((
+      pairs,
+    ) {
+      // A path is valid if we can go from one point to next point i.e.
+      // 1. svr can go to dac, dac can go to fft, and fft can go to out. Or,
+      // 2. svr can go to fft, fft can go to dac, and dac can go to out.
+      return pairs.every((pair) {
+        final (from, to) = pair;
+        return closure[from]!.contains(to);
+      });
+    }).toList();
+    return validPath.fold(1, (previousValue, pair) {
+      final (from, to) = pair;
+      // If there are 'x' ways to go from A to B, and 'y' ways to go from
+      // B to C, then there are 'x * y' ways to go from A to C through B.
+      return previousValue *
+          getNumberOfPaths(connections, from: from, to: to, closure: closure);
+    });
   }
 
-  int getPaths(
+  int getNumberOfPaths(
     Map<String, Set<String>> connections, {
     required String from,
     required String to,
     required Map<String, Set<String>> closure,
-    String? through,
   }) {
     final nextNodes = QueueList.from([from]);
     var validPathsCount = 0;
-    final hasThrough = through != null;
-    final pathsFromThrough = hasThrough
-        ? getPaths(connections, from: through, to: to, closure: closure)
-        : 0;
     while (nextNodes.isNotEmpty) {
       final node = nextNodes.removeFirst();
-      if (hasThrough) {
-        if (node == through) {
-          validPathsCount += pathsFromThrough;
-          continue;
-        }
-      } else {
-        if (node == to) {
-          validPathsCount++;
-          continue;
-        }
+      if (node == to) {
+        validPathsCount++;
+        continue;
       }
+
       if (!closure[node]!.contains(to)) {
+        // If this node cannot reach our destination `to`, ignore this path.
         continue;
       }
       final next = connections[node];
